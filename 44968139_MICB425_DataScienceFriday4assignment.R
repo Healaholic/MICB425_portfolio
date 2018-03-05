4d44a619731f1b30b218f005cc5f548ed5ba0429
Healaholic <ianleeyande@hotmail.com>
2018-03-05 00:07:50 -0800

    Submission of problem set 04

--- 44968139_MICB425_DataScienceFriday4assignment.R
+++ 44968139_MICB425_DataScienceFriday4assignment.R
@@ -215,3 +215,118 @@ metadata %>%
   ggplot() +geom_point(aes(x=Depth_m, y=uM)) + geom_line(aes(x=Depth_m, y=uM)) +
   facet_wrap(~Nutrients_uM, scales="free_y")
 
+
+#Problem set 04
+
+#Part 01
+library(kableExtra)
+library(tidyverse)
+library(knitr)
+
+example_data1 = data.frame(
+    number = c(1,2,3),
+    name = c("lion", "tiger", "bear"),
+    characteristics = c("brown cat", "striped cat", "not a cat"),
+    occurences = c(2, 4, 1)
+  )  
+
+bin1 = data.frame(
+  Number = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15),
+  Name = c("Rigoa","Skittles","MandMs","MikeandIkes","Gummybears","Lego","Gumdrops","fruitgummies","macrophage","cokebottles","Gummywhitedrops","Watermelon","RedGreenFish","Kisses","Redsnakes"),
+  Characteristics = c("long gummies","sour candy with shell","Chocolates with shell","Long chewy beans","Bear shaped gummies","Brick shaped hard candy","large round chewy candy with hard shell","fruit shaped gummies","octopus shaped gummies coated sugar","coke bottle shaped gummies","striped disk gummies coated suger","watermelon coloured and sphere shaped gummies","red and green fish shaped gummies","teardrop shaped chocolates","long thin red snake gummies"),
+  Occurences = c(7,197,218,199,91,18,24,2,6,3,3,1,1,16,13)
+ )  
+  
+bin1 %>% 
+  kable("html") %>%
+  kable_styling(bootstrap_options = "striped", font_size = 10, full_width = F)
+#The "organisms" found in this table takes into account all candy given to us and leaves none out. Rare or unclassifiable species were given their own bin with descriptions on how they differed. 
+
+
+#Part 02
+bin2 = data.frame(
+  x = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141),
+  y = c(1,2,2,3,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6,6,7,7,7,7,7,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8)
+)
+
+ggplot(bin2, aes(x=x, y=y)) +
+  geom_point() +
+  geom_smooth() +
+  labs(x="Cumulative number of individuals classified", y="Cumulative number of species observed")
+#The curve produced is observed to have flattened out after 56 cells have been sampled.
+#The cap on number of species observed is limited by the sampling size: only about half of all species have been observed from this small sample
+
+#Part 03
+
+Spec1 = 7/799
+Spec2 = 197/799
+Spec3 = 218/799
+Spec4 = 199/799
+Spec5 = 91/799
+Spec6 = 18/799
+Spec7 = 24/799
+Spec8 = 2/799
+Spec9 = 6/799
+Spec10 = 3/799
+Spec11 = 3/799
+Spec12 = 1/799
+Spec13 = 1/799
+Spec14 = 16/799
+Spec15 = 13/799
+
+1/(Spec1^2 + Spec2^2 + Spec3^2 + Spec4^2 + Spec5^2 + Spec6^2 + Spec7^2 + Spec8^2 + Spec9^2 + Spec10^2 + Spec11^2 + Spec12^2 + Spec13^2 + Spec14^2 + Spec15^2)
+
+#The simpson reciprocal index for my total original community is 4.706271.
+
+s1 = 31/141
+s2 = 6/141
+s3 = 29/141
+s4 = 46/141
+s5 = 17/141
+s6 = 5/141
+s7 = 3/141
+s8 = 4/141
+
+1/(s1^2+ s2^2 + s3^2 + s4^2 +s5^2 + s6^2 + s7^2 + s8^2)
+#The simpson reciprocal index for my own sample is 4.631027.
+
+8 
+# The chao1 estimate for my sample is 8
+
+15+2^2/26
+
+#The chao1 estimate for the total original community is 15.15385.
+
+#Part04
+library(vegan)
+
+bin1_diversity = 
+  bin1 %>% 
+  select(Name, Occurences) %>% 
+  spread(Name, Occurences)
+
+bin3_diversity = 
+  bin3 %>% 
+  select(Name, Occurences) %>% 
+  spread(Name, Occurences)
+
+bin3 = data.frame(
+  Number = c(1,2,3,4,5,6,7,8),
+  Name = c("Skittles","Bigballs","M&Ms","Jellybeans","Gummybears","Bricks","Kisses","Redsnakes"),
+  Characteristics = c("sour candy with shell","large round chewy candy with hard shell","Chocolates with shell","Long chewy beans","Bear shaped gummies","Brick shaped hard candy","teardrop shaped chocolates","long thin red snake gummies"),
+  Occurences = c(31,6,29,46,17,5,3,4)
+)  
+
+
+diversity(bin1_diversity, index="invsimpson")
+#Simpson Reciprocal Index for the total organism pool is 2.333333.
+specpool(bin1_diversity)
+# Species chao chao.se jack1 jack1.se jack2 boot boot.se n
+#All      15   15       0    15        0    15   15       0 1
+diversity(bin3_diversity, index="invsimpson")
+#Simpson Reciprocal Index for my sample is 4.631027.
+specpool(bin3_diversity)
+#    Species chao chao.se jack1 jack1.se jack2 boot boot.se n
+#All       8    8       0     8        0     8    8       0 1
+
+# The values match the previous calculations
